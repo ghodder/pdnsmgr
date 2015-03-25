2cd2e7e06c1bb118d30254b22f8577e0683584e1
ghodder <geoff@hodder.id.au>
2015-03-25 14:59:06 +1000

    Modified app.blade view to only show menu options after user has logged in

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,17 +34,18 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}">Home</a></li>
+					@if (Auth::check())
+						<li><a href="{{ url('/') }}">Home</a></li>
+						<li><a href="{{ url('/tools') }}">DNS Tools</a></li>
+					@endif
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-					@else
+					@if(Auth::check())
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li class="disabled"><a class="disabled" href="">Profile</a></li>
+								<li class="disabled"><a href="">Profile</a></li>
 								<li class="divider"></li>
 								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
